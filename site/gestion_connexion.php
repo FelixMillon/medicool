aee5d6a0c285ec95f402ca2036e78c30183226b3
felix <felix.millon@outlook.com>
2023-01-16 21:21:05 +0100

    connexion et blocage

--- site/gestion_connexion.php
+++ site/gestion_connexion.php
@@ -14,13 +14,57 @@ require_once("vue/login.php");
 		$unUser = $unControleur->selectWhere($where);
 		if(isset($unUser['email']))
 		{
-			$_SESSION['email'] = $unUser['email'];
-			$_SESSION['nom'] = $unUser['nom'];
-			$_SESSION['prenom'] = $unUser['prenom'];
-			$_SESSION['id'] = $unUser['id'];
-			$_SESSION['droits'] = $unUser['droits'];
-			header("Location: index.php");
+			if($unUser['blocage']=='lock')
+			{
+				echo "<br/> Votre compte est bloqué, veuillez faire une demande de déblocage";
+			}
+			else
+			{
+				$_SESSION['estPatient']=False;
+				$_SESSION['estMedecin']=False;
+				$_SESSION['estSecretaire']=False;
+				$unControleur->setTable("medecin");
+				$LeMedecin = $unControleur->selectWhere($where);
+				if(isset($LeMedecin['email']))
+				{
+					$_SESSION['estMedecin']=True;
+				}
+				$unControleur->setTable("secretaire");
+				$LeSecretaire = $unControleur->selectWhere($where);
+				if(isset($LeSecretaire['email']))
+				{
+					$_SESSION['estSecretaire']=True;
+				}
+				$unControleur->setTable("patient");
+				$lePatient = $unControleur->selectWhere($where);
+				if(isset($lePatient['email']))
+				{
+					$_SESSION['estPatient']=True;
+				}
+				$_SESSION['email'] = $unUser['email'];
+				$_SESSION['nom'] = $unUser['nom'];
+				$_SESSION['prenom'] = $unUser['prenom'];
+				$_SESSION['id'] = $unUser['id'];
+				$_SESSION['droits'] = $unUser['droits'];
+				$tab= array($unUser['id']);
+				$unControleur->callproc('unlockuser',$tab);
+				header("Location: index.php");
+			}
+			
 		}else{
+			$where2 = array('email'=>$_POST['email']);
+			$unUser = $unControleur->selectWhere($where2);
+			if(isset($unUser['id']))
+			{
+				$tab=array(
+					"etat_blocage"=>null, 
+					"nb_essai_restant"=>null, 
+					"date_heure_echec"=>null, 
+					"id_utilisateur"=>$unUser['id']
+					);
+				$unControleur->setTable("nb_echec_co");
+				$unControleur->insert($tab);
+			}
 			echo "<br/> Vérifiez vos identifiants";
 		}
 	}
