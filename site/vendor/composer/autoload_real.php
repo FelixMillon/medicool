647261046f4ab4f8eb4f7d5c910422d8b9bea289
felix <felix.millon@outlook.com>
2023-01-19 21:48:49 +0100

    test et librairies php

--- site/vendor/composer/autoload_real.php
+++ site/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInite250c80cd761a5fc9fd57d443952107a
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInite250c80cd761a5fc9fd57d443952107a::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
